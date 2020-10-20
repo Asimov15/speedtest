45bc0f671b2c28c38760e6eb0b76e37372d720a3
David <zuccaro.david@gmail.com>
2020-10-21 01:30:56 +1100

    login in problems

--- speedtest.py
+++ speedtest.py
@@ -1,17 +1,29 @@
 #!/usr/bin/env python3
-import random
-import os
-import string
-from ftplib import FTP
-import time
+#!/usr/bin/python3
 
-ftp = FTP('34.245.75.246')
-ftp.login(user='awsftpuser', passwd='precopius')
-filename = '/tmp/testfile.txt'
-localfile = open(filename, 'w')
-start = time.time()
-ftp.retrlines('RETR ' + filename, localfile.write)
-end = time.time()
-print(end - start)
-ftp.quit()
-localfile.close()
+import ftplib 
+import os
+
+with ftplib.FTP('34.245.75.246', 'awsftpuser', 'precopius') as ftp:
+
+	file_orig = '/testfile.txt'
+	file_copy = '/tmp/testfile.txt'
+
+	try:
+		ftp.login()  
+		
+		with open(file_copy, 'w') as fp:
+			
+			res = ftp.retrlines('RETR ' + file_orig, fp.write)
+			
+			if not res.startswith('226 Transfer complete'):
+				
+				print('Download failed')
+				if os.path.isfile(file_copy):
+					os.remove(file_copy)
+
+	except ftplib.all_errors as e:
+		print('FTP error:', e) 
+		
+		if os.path.isfile(file_copy):
+			os.remove(file_copy)
