d516ae850f5350053380908489060076ecc76d3e
David <zuccaro.david@gmail.com>
2020-10-21 13:59:19 +1100

    use ftp

--- create_file.py
+++ create_file.py
@@ -1,9 +1,15 @@
+#!/usr/bin/env python3
+
+import random
 import string
 
-f = open(testfile, "a")
-for i in range(int(1e4)):
+testfile = "testfile.txt"
+
+f = open(testfile, "w")
+for i in range(int(1e5)):
 	line = ""
 	for j in range(100):
 		line = line + random.choice(string.ascii_letters)
+	line = line + '\n'
 	f.write(line)
 f.close()
--- speedtest.py
+++ speedtest.py
@@ -1,19 +1,51 @@
 #!/usr/bin/env python3
 
+import ftplib 
+import os
+import string
 import time
-from ftplib import FTP
+import shutil
+import random
 
-ftp = FTP('34.245.75.246')
+file_orig = 'testfile.txt'
+file_copy = '/tmp/testfile.txt'
+location  = 'Ireland'
 
-ftp.login('awsftpuser','precopius')
+with ftplib.FTP('34.245.75.246') as ftp:
 
-filename = '/tmp/testfile.txt'
-downfile = 'testfile.txt'
+	ftp.set_pasv(False)
 
-localfile = open(filename, 'wb')
-start = time.time()
-ftp.retrbinary('RETR ' + downfile, localfile, 1024) 
-ftp.quit()
-end = time.time()
-print(end - start)
-localfile.close()
+	try:
+		ftp.login('awsftpuser', 'precopius')  
+		
+		with open(file_copy, 'wb') as fp:
+			start = time.time()
+			result = ftp.retrbinary('RETR ' + file_orig, fp.write)
+			end = time.time()
+			if not result.startswith('226 Transfer complete'):
+				print('Download failed')
+				if os.path.isfile(file_copy):
+					os.remove(file_copy)
+
+	except ftplib.all_errors as e:
+		print('FTP error:', e)
+		if os.path.isfile(file_copy):
+			os.remove(file_copy)
+
+	mbs = 10100000 / (1e6 * (end-start))
+	print("Download Speed From {1} AWS: {0:0.4} Megabytes per second".format(mbs, location))
+
+	newfn = ""
+	for j in range(10):
+		newfn = newfn + random.choice(string.ascii_letters)
+
+	newfn = "./" + newfn + ".st"
+	shutil.move(file_copy, newfn)
+	upfile = open(newfn,'rb')
+	start = time.time()
+	result = ftp.storbinary('STOR ' + newfn, upfile)
+	end = time.time()
+
+	mbs = 10100000 / (1e6 * (end-start))
+	print("Upload Speed To {1} AWS: {0:0.4} Megabytes per second".format(mbs, location))
+
