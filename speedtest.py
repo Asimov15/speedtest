862a13cf295660e9f53973076fed1709ae2e52c7
David <zuccaro.david@gmail.com>
2020-10-27 04:03:27 +1100

    use sftp

--- speedtest.py
+++ speedtest.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-import ftplib 
 import os
 import string
 import time
@@ -11,6 +10,7 @@ from pathlib import Path
 import mysql.connector
 from mysql.connector import Error
 from datetime import datetime
+import pysftp
 
 client_id = '00001'
 send_dir = 'send/'
@@ -26,38 +26,28 @@ def testspeed(ip, location):
 	suffix = "tf" + timestampstr + ".txt"
 	newfn = send_dir + suffix
 	copyfile(testfile, newfn)
-	ftp = ftplib.FTP(ip)
-	ftp.set_pasv(False)
-	ftp.login('awsftpuser', 'precopius')
-	fp = open(newfn, 'rb')
-	start = time.time()
-	result = ftp.storbinary('STOR ' + suffix, fp)
-	end = time.time()
-	fp.close()
-	if not result.startswith('226 Transfer complete'):
-		print('upload failed')
-	else:
+	with pysftp.Connection(ip, username='awsftpuser', password='precopius') as sftp:
+		start = time.time()
+		sftp.put(newfn) 
+		end = time.time()
 		os.remove(newfn)
 		mbs_up = 1000000 / (1e6 * (end-start))
 		print("Up Speed To {1} AWS: {0:0.4} Megabytes per second".format(mbs_up, location))
-		newfn = receive_dir + suffix
-		fp = open(newfn,'wb')
+		newfn = suffix
 		start = time.time()
-		result = ftp.retrbinary('RETR ' + suffix, fp.write)
-		if not result.startswith('226 Transfer complete'):
-			print('Download failed')
-		else:
-			end = time.time()
-			mbs_down = 1000000 / (1e6 * (end - start))
-			print("Download Speed To {1} AWS: {0:0.4} Megabytes per second".format(mbs_down, location))
-			mydb = mysql.connector.connect(host="speedtester.c6gl0ojvhvge.ap-southeast-2.rds.amazonaws.com", user="dz", password="precopius", database="speedtester")
-			mycursor = mydb.cursor()
-			sql = """INSERT INTO speed_data (client_id, destination, upload_speed, download_speed, datetime ) VALUES (%s, %s, %s, %s, %s)"""
-			recordtuple = (client_id, location, mbs_up, mbs_down, datetime.utcnow())
-			mycursor.execute(sql, recordtuple)
-			mydb.commit()
+		localpath = receive_dir + newfn
+		sftp.get(newfn, localpath)
+		end = time.time()
+		mbs_down = 1000000 / (1e6 * (end - start))
+		print("Download Speed To {1} AWS: {0:0.4} Megabytes per second".format(mbs_down, location))
+		mydb = mysql.connector.connect(host="speedtester.c6gl0ojvhvge.ap-southeast-2.rds.amazonaws.com", user="dz", password="precopius", database="speedtester")
+		mycursor = mydb.cursor()
+		sql = """INSERT INTO speed_data (client_id, destination, upload_speed, download_speed, datetime ) VALUES (%s, %s, %s, %s, %s)"""
+		recordtuple = (client_id, location, mbs_up, mbs_down, datetime.utcnow())
+		mycursor.execute(sql, recordtuple)
+		mydb.commit()
 
-		os.remove(newfn)
+		os.remove(localpath)
 
 if not os.path.exists(send_dir):
 	os.mkdir(send_dir)
@@ -78,7 +68,8 @@ if not os.path.exists(testfile):
 		f.write(line)
 	f.close()
 
-for i in range(100):
+for i in range(int(1e5)):
 	#testspeed('34.245.75.246', 'Ireland')
-	testspeed('54.157.129.155', 'North Virginia') 
+	testspeed('54.157.129.155', 'North Virginia')
+	time.sleep(10 * 60) 
 
